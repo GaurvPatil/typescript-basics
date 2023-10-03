28a5e4c876b56cb6c2c6c34586a294fa994444b1
GaurvPatil <gaurvpatil17@gmail.com>
2023-10-03 11:57:28 +0530

    hello world

---
+++ 01Intro/intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "gaurav",
+    age: 23,
+};
+console.log("gaurav");
+console.log(user.name);
---
+++ 01Intro/intro.ts
@@ -0,0 +1,7 @@
+let user = {
+  name: "gaurav",
+  age: 23,
+};
+
+console.log("gaurav");
+console.log(user.name);
