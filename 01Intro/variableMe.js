31fe65c740a9f859f1b27b840055a595f9ac6401
GaurvPatil <gaurvpatil17@gmail.com>
2023-10-03 13:45:38 +0530

    number n boolean , we don't need to explicitly mention type of varibale if it is self explementary

--- 01Intro/variableMe.js
+++ 01Intro/variableMe.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Gaurav";
 console.log(greetings);
+//number
+// let userID : number = 2350
+var userID = 2350;
+//boolean
+var isLoggeIn = false;
--- 01Intro/variableMe.ts
+++ 01Intro/variableMe.ts
@@ -1,4 +1,13 @@
 let greetings : string = "Hello Gaurav";
 console.log(greetings);
 
+//number
+// let userID : number = 2350
+let userID  = 2350
+
+//boolean
+let isLoggeIn : boolean = false
+
+
+
 export{}
