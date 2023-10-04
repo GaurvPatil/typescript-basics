322d9cdfc55779c73527eeb8ceadbbac38e63de9
GaurvPatil <gaurvpatil17@gmail.com>
2023-10-04 14:47:37 +0530

    function return type , functions without return values

--- 02Basics/myFunctions.js
+++ 02Basics/myFunctions.js
@@ -18,4 +18,20 @@ getUpper("gaurav");
 signUp("gaurav", "gaurav@co.dev", false);
 // default values
 loginUser("gaurav", "gaurav@co.dev");
+// lecture - 2
+function addNum(num) {
+    return num + 2;
+}
+var getString = function (s) {
+    return " ";
+};
+// functions return nothing
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
+// function return type 
+addNum(2);
 // in case of varibales  , it's optional . it inferce the type pretty nicely . so there is no problem . In the case of function , it is rally compulsory
--- 02Basics/myFunctions.ts
+++ 02Basics/myFunctions.ts
@@ -10,6 +10,9 @@ function signUp(name: string, email: string, isPaid: boolean) {}
 
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
+
+
+
 // number type
 addTwo(5);
 
@@ -21,6 +24,32 @@ signUp("gaurav", "gaurav@co.dev", false);
 
 // default values
 loginUser("gaurav", "gaurav@co.dev");
+
+
+// lecture - 2
+
+function addNum(num : number): number{
+    return num + 2
+}
+
+
+const getString = (s:string) : string =>{
+    return " "
+}
+
+// functions return nothing
+
+function consoleError(errmsg : string): void{
+    console.log(errmsg)
+}
+
+function handleError(errmsg : string): never{
+    throw new Error(errmsg)
+}
+
+
+// function return type 
+addNum(2);
 
 export {};
 
