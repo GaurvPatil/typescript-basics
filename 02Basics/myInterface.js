26984fc71e9d3ea237d432eded63587b516ee915
GaurvPatil <gaurvpatil17@gmail.com>
2023-10-10 18:12:30 +0530

    more about Interface

--- 02Basics/myInterface.js
+++ 02Basics/myInterface.js
@@ -3,6 +3,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var gaurav = {
     dbId: 33,
+    githubToken: "github",
+    emailId: "g@g.com",
+    userId: 2356,
+    startTrail: function () {
+        return "hello gaurav";
+    },
+    getCoupon: function (name, off) {
+        return 10;
+    },
+};
+var ramesh = {
+    dbId: 33,
+    githubToken: "github",
+    role: "admin",
     emailId: "g@g.com",
     userId: 2356,
     startTrail: function () {
--- 02Basics/myInterface.ts
+++ 02Basics/myInterface.ts
@@ -13,8 +13,35 @@ interface User {
   getCoupon(coupanName: string, value: number): number;
 }
 
+interface User {
+  githubToken: string;
+}
+
+// inheritance
+// we can extend multiple interfces
+// example = interface Admin extends User , Another , Xyz {...}
+interface Admin extends User {
+  role: "admin" | "ta" | "learner";
+}
+
 const gaurav: User = {
   dbId: 33,
+  githubToken: "github",
+  emailId: "g@g.com",
+  userId: 2356,
+  startTrail: () => {
+    return "hello gaurav";
+  },
+
+  getCoupon: (name: "gaurav13", off: 10) => {
+    return 10;
+  },
+};
+
+const ramesh: Admin = {
+  dbId: 33,
+  githubToken: "github",
+  role: "admin",
   emailId: "g@g.com",
   userId: 2356,
   startTrail: () => {
