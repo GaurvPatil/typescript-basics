9fee121b4697850f509065001eba29022e81b3f9
GaurvPatil <gaurvpatil17@gmail.com>
2023-10-12 11:28:03 +0530

    getters , setters and private methods

--- 03Purets/dist/classes.js
+++ 03Purets/dist/classes.js
@@ -13,12 +13,30 @@ class User {
 const Gaurav = new User("g@g.com", "gaurav");
 Gaurav.city = "Banglore";
 // Gaurav.height = not accesable
-// other syntax  = better way 
+// other syntax  = better way
 class User2 {
     constructor(email, name, userId) {
         this.email = email;
         this.name = name;
         this.userId = userId;
         this.city = "Pune";
+        this._courseCount = 1;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    // there are private method also 
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    //   in settters there is no return type not even VOID 
+    set courseCount(coursenum) {
+        if (coursenum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = coursenum;
     }
 }
--- 03Purets/src/classes.ts
+++ 03Purets/src/classes.ts
@@ -1,29 +1,55 @@
 class User {
-    email: string;
-    name: string;
-    city: string = "Pune";
-    // read only
-    readonly bloodGroup: string = "B+";
-    // private = not accesable .  default Public
-    private height: number = 5.9;
-    constructor(email: string, name: string) {
-      this.email = email;
-      this.name = name;
+  email: string;
+  name: string;
+  city: string = "Pune";
+  // read only
+  readonly bloodGroup: string = "B+";
+  // private = not accesable .  default Public
+  private height: number = 5.9;
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const Gaurav = new User("g@g.com", "gaurav");
+Gaurav.city = "Banglore";
+
+// Gaurav.height = not accesable
+
+// other syntax  = better way
+
+
+
+
+class User2 {
+  readonly city: string = "Pune";
+  private _courseCount: number = 1;
+  constructor(
+    public email: string,
+    public name: string,
+    private userId: string
+  ) {}
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+
+// there are private method also 
+private deleteToken(){
+    console.log("token deleted")
+}
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+
+//   in settters there is no return type not even VOID 
+  set courseCount(coursenum: number) {
+    if (coursenum <= 1) {
+      throw new Error("course count should be more than 1");
     }
+    this._courseCount = coursenum;
   }
-  
-  const Gaurav = new User("g@g.com", "gaurav");
-  Gaurav.city = "Banglore";
-  
-  // Gaurav.height = not accesable
-  
-  // other syntax  = better way 
-  
-  class User2{
-      readonly city : string = "Pune"
-      constructor(
-          public email : string,
-          public name : string,
-          private userId : string
-      ){}
-  }
+}
